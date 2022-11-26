6526841ea5c91629dcb076df43c0d38a6bbe1e05
Юлия Ц <107360558+JulietteT@users.noreply.github.com>
2022-11-26 13:26:07 +0500

    Update Radio.java

--- src/main/java/ru/netology/domain/Radio.java
+++ src/main/java/ru/netology/domain/Radio.java
@@ -1,74 +1,72 @@
 package ru.netology.domain;
 
 public class Radio {
-    private int maxVolume = 10;
-    private int minVolume = 0;
-    private int maxChannel = 9;
-    private int minChannel = 0;
-    private int currentVolume;
-    private int currentChannel;
+    final int maxStation;
+    static final int minStation = 0;
+    private int currentStation;
+    static final int maxVolume = 100;
+    static final int minVolume = 0;
+    private int currentVolume = 30;
 
-    public void increaseVolume() {
-        if (currentVolume == maxVolume) {
-            return;
-        }
-        currentVolume++;
+    public Radio() { //по умолчанию
+        this.maxStation = 9;
     }
 
-    public void decreaseVolume() {
-        if (currentVolume == minVolume) {
-            return;
-        }
-        currentVolume--;
+    public Radio(int counterStation) { //изменения кол-ва станций
+        this.maxStation = counterStation - 1;
     }
 
-    public void increaseChannel() {
-        if (currentChannel == maxChannel) {
-            this.currentChannel = minChannel;
-            return;
-        }
-        currentChannel++;
+    public int getCurrentStation() {
+        return currentStation;
     }
 
-    public void decreaseChannel() {
-        if (currentChannel == minChannel) {
-            this.currentChannel = maxChannel;
+    public void setCurrentStation(int currentStation) {
+        if (currentStation < minStation || currentStation > maxStation) {
             return;
         }
-        currentChannel--;
+        this.currentStation = currentStation;
     }
 
+    public void nextStation() { //след станция
+        if (currentStation == maxStation) {
+            this.currentStation = minStation;
+        } else {
+            this.currentStation = currentStation + 1;
+        }
+    }
+
+    public void prevStation() { //пред станция
+        if (currentStation == minStation) {
+            this.currentStation = maxStation;
+        } else {
+            this.currentStation = currentStation - 1;
+        }
+    }
 
     public int getCurrentVolume() {
         return currentVolume;
     }
 
     public void setCurrentVolume(int currentVolume) {
-        if (currentVolume > maxVolume) {
-            this.currentVolume = maxVolume;
+        if (currentVolume < minVolume || currentVolume > maxVolume) {
             return;
+        } else {
+            this.currentVolume = currentVolume;
         }
-        if (currentVolume < minVolume) {
-            this.currentVolume = minVolume;
-            return;
-        }
-        this.currentVolume = currentVolume;
     }
 
-
-    public void setCurrentChannel(int currentChannel) {
-        if (currentChannel > maxChannel) {
-
+    public void volumeUp() { //повышение громкости
+        if (currentVolume == maxVolume) {
             return;
         }
-        if (currentChannel < minChannel) {
-
-            return;
-        }
-        this.currentChannel = currentChannel;
+        this.currentVolume = currentVolume + 1;
     }
 
-    public int getCurrentChannel() {
-        return currentChannel;
+    public void volumeDown() { //понижение громкости
+        if (currentVolume == minVolume) {
+            return;
+        }
+        this.currentVolume = currentVolume - 1;
     }
 }
+   
